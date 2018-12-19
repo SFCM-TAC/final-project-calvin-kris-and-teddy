2a0a36bbca21f796b0eaa52095bd290e42a6de08
Teddy Raven <teddyraven@Teddys-MacBook-Pro.local>
2018-12-18 22:18:51 -0800

    made and included music assets

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ var battleStage = 0;
 var damage;
 var damageModifier = 6;
 
+
 // Move the player in a specified direction.
 function move(direction){
   switch (direction){
@@ -86,7 +87,8 @@ function updatePage(){
   if ((xPosition == 0 && yPosition == 0) || (xPosition == 0 && yPosition == 1) || (xPosition == 0 && yPosition == 2) || (xPosition == 0 && yPosition == 3) || (xPosition == 0 && yPosition == 8) || (xPosition == 1 && yPosition == 0) || (xPosition == 1 && yPosition == 8) || (xPosition == 4 && yPosition == 2) || (xPosition == 5 && yPosition == 1) || (xPosition == 6 && yPosition == 0) || (xPosition == 6 && yPosition == 1) || (xPosition == 6 && yPosition == 2) || (xPosition == 7 && yPosition == 0) || (xPosition == 7 && yPosition == 1) || (xPosition == 8 && yPosition == 0) || (xPosition == 8 && yPosition == 1) || (xPosition == 8 && yPosition == 2)){
     //ocean
 
-
+    title();
+    console.log("title");
     document.getElementById("gameInput").innerHTML = "Treading water a mile out from shore, you wonder why the hell you decided to swim out into the ocean.";
   }else if ((xPosition == 1 && yPosition == 1) || (xPosition == 1 && yPosition == 2) || (xPosition == 1 && yPosition == 4) || (xPosition == 2 && yPosition == 0) || (xPosition == 3 && yPosition == 0) || (xPosition == 3 && yPosition == 1) || (xPosition == 3 && yPosition == 2) || (xPosition == 4 && yPosition == 0) || (xPosition == 4 && yPosition == 1) || (xPosition == 4 && yPosition == 3) || (xPosition == 4 && yPosition == 8) || (xPosition == 5 && yPosition == 0) || (xPosition == 5 && yPosition == 3) || (xPosition == 5 && yPosition == 8) || (xPosition == 6 && yPosition == 3) || (xPosition == 7 && yPosition == 2) || (xPosition == 8 && yPosition == 3) || (xPosition == 8 && yPosition == 5) || (xPosition == 8 && yPosition == 6) || (xPosition == 8 && yPosition == 7) || (xPosition == 8 && yPosition == 8)){
     //shore
@@ -96,38 +98,47 @@ function updatePage(){
   }else if ((xPosition == 2 && yPosition == 2) || (xPosition == 2 && yPosition == 3) || (xPosition == 3 && yPosition == 4) || (xPosition == 4 && yPosition == 4) || (xPosition == 5 && yPosition == 6) || (xPosition == 6 && yPosition == 5)){
     //plains
     enemyClass = "goblins";
+    goblin();
     document.getElementById("gameInput").innerHTML = "A vast, golden expanse stretching before you, your horse kicks some dirt up with its front hoof, eager for the chance to stretch its legs.";
   }else if ((xPosition == 1 && yPosition == 5) || (xPosition == 1 && yPosition == 6) || (xPosition == 2 && yPosition == 4) || (xPosition == 2 && yPosition == 6) || (xPosition == 3 && yPosition == 5) || (xPosition == 3 && yPosition == 6) || (xPosition == 3 && yPosition == 7) || (xPosition == 5 && yPosition == 7) || (xPosition == 6 && yPosition == 6) || (xPosition == 6 && yPosition == 8) || (xPosition == 7 && yPosition == 6) || (xPosition == 7 && yPosition == 7) || (xPosition == 7 && yPosition == 8)){
     //mountains
     enemyClass = "wolves";
+    travel();
     document.getElementById("gameInput").innerHTML = "Carving switchbacks into scree, stone and eventually ice, you slowly make your way up the mountainside. The air is thin as you balance on a thin mountain ridge. Your hands and feet feel numb.";
   }else if ((xPosition == 0 && yPosition == 4) || (xPosition == 0 && yPosition == 5) || (xPosition == 0 && yPosition == 6) || (xPosition == 0 && yPosition == 7) || (xPosition == 1 && yPosition == 7) || (xPosition == 2 && yPosition == 7) || (xPosition == 2 && yPosition == 8)){
     //fjords
     enemyClass = "drakes";
+    travel();
     document.getElementById("gameInput").innerHTML = "You scramble down a rocky cliff face into a deep fjord. Reaching the water, the prospect of scaling the other side grows increasingly daunting.";
   }else if ((xPosition == 6 && yPosition == 4) || (xPosition == 7 && yPosition == 3) || (xPosition == 7 && yPosition == 4) || (xPosition == 7 && yPosition == 5)){
     //forest
     enemyClass = "elves";
+    travel();
     document.getElementById("gameInput").innerHTML = "The trees grow denser as you journey into the heart of the woods. As you pass by, you could swear some of them have started leaning towards you.";
   }else if ((xPosition == 4 && yPosition == 5) || (xPosition == 4 && yPosition == 6)){
     //river
     enemyClass = "thieves";
+    travel();
     document.getElementById("gameInput").innerHTML = "Walking the bank, you decide to follow the river for a while. The pressures of navigation gone, you can finally take the time to reflect on your travels.";
   }else if (xPosition == 1 && yPosition == 3){
     //island city
     enemyClass = "guard";
+    goblin();
     document.getElementById("gameInput").innerHTML = "Traversing a long, crumbling bridge, you're confronted by a rather angry looking guard wearing a ceremonial helmet almost as tall as he is. He topheavily teeters towards you palm outstretched, demanding you pay the toll.";
   }else if (xPosition == 3 && yPosition == 3){
     //capital city
     enemyClass = "thieves";
+    treasure();
     document.getElementById("gameInput").innerHTML = "You could see it from miles off, and even before you reached it, you knew that this town must be important. High, crenelated walls and towering bastions make the city's hilltop position all the more impressive. Trotting through the front gates, you and your horse notice a large banner that reads 'Welcome to the biannual map festival!'";
   }else if (xPosition == 5 && yPosition == 4){
     //lake city
     enemyClass = "thieves";
+    treasure();
     document.getElementById("gameInput").innerHTML = "As you approach the city, the sounds of merchants proselytizing their wares and water rushing through stone canals blends together. Woodsmen from up the river maneuver logs through the canals for delevery to the bay where they'll be sold off to god knows where.";
   }else if (xPosition == 4 && yPosition == 7){
     //cliff city
     enemyClass = "thieves";
+    title();
     document.getElementById("gameInput").innerHTML = "How do all those peoples' houses not fall off that big-ass clif!?";
     document.getElementById("image").src = "assets/cliffVillage.png";
   }else if (xPosition == 8 && yPosition == 4){
@@ -141,15 +152,18 @@ function updatePage(){
   }else if (xPosition == 6 && yPosition == 7){
     //boss tile
     enemyClass = "boss";
+    goblin();
     document.getElementById("gameInput").innerHTML = "Jeepers creepers, that's a big dragon!!!";
   }else if (xPosition == 5 && yPosition == 2){
     //mystery island
+    goblin();
     document.getElementById("gameInput").innerHTML = "Woah, this's a spooky island!!!";
   }else if (xPosition == 5 && yPosition == 5){
     //lake
     document.getElementById("gameInput").innerHTML = "That lake sure has a bunch of crud in it!!!";
   }else{
     //out to sea
+    goblin();
     document.getElementById("gameInput").innerHTML = "You're gonna drown!!!";
     health --;
   }
--- sounds.js
+++ sounds.js
@@ -6,40 +6,106 @@
 
 // var synth = new Tone.Synth().toMaster()
 // synth.triggerAttackRelease('C4', '8n')
+function title(){
+var player = new Tone.Player("music/Opening title.mp3").toMaster();
+player.autostart = true;}
+console.log("player");
+
+function goblin(){
+  var player = new Tone.Player("music/Goblin.mp3").toMaster();
+  player.autostart = true;}
+}
+
+function travel(){
+  var player = new Tone.Player("music/Travel.mp3").toMaster();
+  player.autostart = true;}
+}
+
+function treasure(){
+  var player = new Tone.Player("music/Treasure.mp3").toMaster();
+  player.autostart = true;}
+}
 
 function sound() {
+var synth = new Tone.Synth().toMaster();
+synth.triggerAttackRelease('C4', '4n', '8n')
+synth.triggerAttackRelease('Eb4', '8n', Tone.Time('4n') + Tone.Time('8n'))
+synth.triggerAttackRelease('D4', '16n', '2n')
+synth.triggerAttackRelease('C4', '16n', Tone.Time('2n') + Tone.Time('8t'))
+synth.triggerAttackRelease('Bb3', '16', Tone.Time('2n') + Tone.Time('8t')*2)
+synth.triggerAttackRelease('C4', '2n', Tone.Time('2n') + Tone.Time('8t'))
+synth.triggerAttackRelease('G3', '2n', '0:3')}
+
+
+
 // var crusher = new Tone.BitCrusher(8).toMaster();
-  var synth = new Tone.AMSynth({
+//   var synth = new Tone.AMSynth({
+//
+// 		harmonicity  : 3 ,
+// 		detune  : 0 ,
+// 		oscillator  : {
+// 		// type  : sine
+// 		}  ,
+// 		envelope  : {
+// 		attack  : 0.01 ,
+// 		decay  : 0.01 ,
+// 		sustain  : 1 ,
+// 		release  : 0.5
+// 		}  ,
+// 		modulation  : {
+// 		// type  : square
+// 		}  ,
+// 		modulationEnvelope  : {
+// 		attack  : 0.5 ,
+// 		decay  : 0 ,
+// 		sustain  : 1 ,
+// 		release  : 0.5
+// 		}
+//
+// }
+// ).toMaster();
+// // synth.triggerAttackRelease(['C4', '4n'],['Eb4', '4n']);
+// // synth.triggerAttackRelease('Eb4', '4n');
+// // synth.triggerAttackRelease('D4', '8n');
+// // synth.triggerAttackRelease('C4', '8n');
+// // synth.triggerAttackRelease('Bb3', '4n');
+// // synth.triggerAttackRelease('C4', '4n');
+// var seq = new Tone.Sequence(function(time, note){
+// 	console.log(note);
+// //straight quater notes
+// }, ["C4", "E4", "G4", "A4"], "4n").;
 
-		harmonicity  : 3 ,
-		detune  : 0 ,
-		oscillator  : {
-		// type  : sine
-		}  ,
-		envelope  : {
-		attack  : 0.01 ,
-		decay  : 0.01 ,
-		sustain  : 1 ,
-		release  : 0.5
-		}  ,
-		modulation  : {
-		// type  : square
-		}  ,
-		modulationEnvelope  : {
-		attack  : 0.5 ,
-		decay  : 0 ,
-		sustain  : 1 ,
-		release  : 0.5
-		}
 
-}
-).toMaster();
-synth.triggerAttackRelease('C4', '4n');
 
 // var seq = new Tone.Sequence(function(time, note){
 // 	console.log(note);
 // //subdivisions are given as subarrays
 // }, ["C4", ["E4", "D4", "E4"], "G4", ["A4", "G4"]]);
 // synth.seq.toMaster();
-
-}
+// var synth = new Tone.Synth().toMaster()
+//
+// //pass in an array of events
+// var part = new Tone.Part(function(time, event){
+// 	//the events will be given to the callback with the time they occur
+// 	synth.triggerAttackRelease(event.note, event.dur, time)
+// }, [{ time : 0, note : 'C4', dur : '4n'},
+// 	{ time : '4n + 8n', note : 'E4', dur : '8n'},
+// 	{ time : '2n', note : 'G4', dur : '16n'},
+// 	{ time : '2n + 8t', note : 'B4', dur : '4n'}])
+//
+// //start the part at the beginning of the Transport's timeline
+// // part.start(0)
+// //
+// // //loop the part 3 times
+// // part.loop = 3
+// // part.loopEnd = '1m'
+//
+// // document.querySelector('.playToggle').addEventListener('change', function(e){
+// // 	if (e.target.checked){
+// // 		Tone.Transport.start('+0.1')
+// // 	} else {
+// // 		Tone.Transport.stop()
+// // 	}
+// // })
+//
+// }
